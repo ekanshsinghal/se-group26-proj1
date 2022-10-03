d11b5ec9f2a50967cd1cc82010bfc951b866bea5
gowtham-sathyan <gowthamsathyan@gmail.com>
2022-10-02 22:25:53 -0400

    Added add, modify, delete and view APIs

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, url_for, redirect, session
-from pymongo import MongoClient
+from flask import Flask, render_template, request, url_for, redirect, session, jsonify
+from pymongo import MongoClient, ReturnDocument
 import bcrypt
 import datetime
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.secret_key = "testing"
@@ -10,7 +11,6 @@ client = MongoClient("mongodb+srv://se_test_user:se_test_user123@cluster0.npdzip
 db = client.get_database("Test")
 UserRecords = db.register
 Applications = db.Applications
-
 
 @app.route("/", methods=["post", "get"])
 def register():
@@ -105,15 +105,43 @@ def add():
         return redirect(url_for("login"))
 
 
-@app.route("/view")
+@app.route("/view", methods=["POST"])
 def view():
+    payload={"status":False,"msg":"","Applications":[]}
+
+    #Use this if not using session
+
+    # email = request.form["email"]
+    # out = Applications.find({"Email": email})
+    # if out:
+    #     payload["status"]=True
+    #     payload["msg"]="Applications present"
+    #     for i in out:
+    #         del i['_id']
+    #         del i['Email']
+    #         payload.append(i)
+    # else:
+    #     payload["msg"]="No Applications / Email does not exist"
+    # return jsonify(payload),200
+
+
+    #IF using session, use this:
+
     if "email" in session:
+        payload["status"]=True
         email = session["email"]
         out = Applications.find({"Email": email})
-        # print("!!!!!!!!!!!!", type(out))
-        return render_template("display_applications.html", apps=out)
+        if out:
+            payload["msg"]="Applications present"
+            for i in out:
+                del i['Email']
+                i['_id']=str(i['_id'])
+                payload["Applications"].append(i)
+        else:
+            payload["msg"]="No Applications"
     else:
-        return redirect(url_for("login"))
+        payload["msg"]="Session timed out"
+    return jsonify(payload),200
 
 @app.route("/add_application", methods=["POST", "GET"])
 def add_application():
@@ -145,10 +173,90 @@ def add_application():
             "Status": status
         }
         Applications.insert_one(application)
-
-        return render_template("home.html", email=email)
+        res={"msg":"Addition successful"}
+        return jsonify(res),200
+        # return render_template("home.html", email=email)
     else:
-        return redirect(url_for("login"))
+        res={"msg":"Session timed out"}
+        return jsonify(res),200
+
+
+@app.route("/delete", methods=["POST"])
+def delete_application():
+    payload = {"status":False,"msg":""}
+
+    #Use this if not using session
+
+    # application_id = request.form["_id"]
+    # email = request.form["email"]
+    # delete_document = Applications.find_one_and_delete({"_id":ObjectId(application_id), "Email":email})
+    # if delete_document == None:
+    #     payload["msg"] = "No such application id or email"
+    # else:
+    #     payload["status"] = True
+    #     payload["msg"] = "Deleted application"
+    # return jsonify(payload),200
+
+
+    #IF using session, use this:
+    if "email" in session:
+        email = session["email"]
+        application_id = request.form["_id"]
+        delete_document = Applications.find_one_and_delete({"_id":ObjectID(application_id), "Email":email})
+        if delete_document == None:
+            payload["msg"] = "No such application id or email"
+        else:
+            payload["status"] = True
+            payload["msg"] = "Deleted application"
+    else:
+        payload["msg"] = "Session timed out"
+
+    return jsonify(payload),200
+
+
+@app.route("/modify", methods=["POST"])
+def modify_application():
+    payload = {"status":False,"msg":""}
+
+
+    #Use this if not using session
+    
+    # application_id = request.form["_id"]
+    # email = request.form["email"]
+    # filter = {'_id':ObjectId(application_id), "email": email}
+    # set_values = {"$set":{}}
+    # for i in request.form:
+    #     if i!="_id" and i!="email":
+    #         set_values["$set"][i] = request.form[i]
+    # modify_document = Application.find_one_and_update(filter, set_values, return_document = ReturnDocument.AFTER)
+    # if modify_document == None:
+    #     payload["msg"] = "No such application id or email"
+    # else:
+    #     payload["status"] = True
+    #     payload["msg"] = "Modified application"
+    # return jsonify(payload),200
+
+
+    #IF using session, use this:
+    
+    if "email" in session:
+        application_id = request.form["_id"]
+        email = request.form["email"]
+        filter = {'_id':ObjectId(application_id), "email": email}
+        set_values = {"$set":{}}
+        for i in request.form:
+            if i!="_id" and i!="email":
+                set_values["$set"][i] = request.form[i]
+        modify_document = Application.find_one_and_update(filter, set_values, return_document = ReturnDocument.AFTER)
+        if modify_document == None:
+            payload["msg"] = "No such application id or email"
+        else:
+            payload["status"] = True
+            payload["msg"] = "Modified application"
+    else:
+        payload["msg"] = "Session timed out"
+
+    return jsonify(payload),200
 
 
 if __name__ == "__main__":
