5eff8096ff6a7a36a7644141fbe50debcde25c47
Rahul RK <rahulrk.2303@gmail.com>
2022-10-07 20:01:25 -0400

    Added backend login and register test

--- backend/tests.py
+++ backend/tests.py
@@ -1,20 +1,48 @@
 import requests
+import pprint
+import json
 
 class TestBackend:
     def __init__(self):
         self.fails = 0
         self.passed = 0
-        self.serverURL = "http://127.0.0.1:8000/"
+        self.serverURL = "http://192.168.0.142:8000"
 
-    def runTests(slef):
-        pass
+    def runTests(self):
+        self.testLogin()
+        self.testRegister()
 
     def testLogin(self):
-        email = "user@ncsu.edu".encode()
-        password = "12345678".encode()
-        r = requests.get(url = self.serverURL, data = {email, password})
-        if r.status_code == 404:
+        req = {}
+        req["email"] = "rrangar@ncsu.edu"
+        req["password"] = "12345678"
+        urlToSend = self.serverURL + "/login"
+        r = requests.post(url = urlToSend, json = req)
+        if r.status_code == 200:
             self.passed += 1
-        
+            print("Login Passed")
+        elif r.status_code == 400:
+            self.fails += 1
+            print("Login Failed")
+            pprint.pprint(json.loads(r.content))
+
+    def testRegister(self):
+        req = {}
+        req["firstName"] = "Rahul"
+        req["lastName"] = "RK"
+        req["email"] = "rrangar2@ncsu.edu"
+        req["password"] = "12345678"
+        req["confirmPassword"] = "12345678"
+        urlToSend = self.serverURL + "/register"
+        r = requests.post(url = urlToSend, json = req)
+        if r.status_code == 200:
+            self.passed += 1
+            print("Register Passed")
+        elif r.status_code == 400:
+            self.fails += 1
+            print("Register Failed")
+            pprint.pprint(json.loads(r.content))
+
+
 testObj = TestBackend()
-testObj.testLogin()
+testObj.runTests()
