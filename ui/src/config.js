1733335b33f6163c862f0235f5e751fb0504fa10
Jayraj Mulani <jayrajm.mulani@gmail.com>
2022-11-26 23:05:13 -0500

    Add debug instructions

--- ui/src/config.js
+++ ui/src/config.js
@@ -1,4 +1,7 @@
 const config = {
+	// Comment this when debugging locally
 	base_url: 'http://ec2-65-1-94-52.ap-south-1.compute.amazonaws.com:8000',
+	// Uncomment this when debugging locally
+	// base_url: 'http://localhost:8000',
 };
 export default config;
